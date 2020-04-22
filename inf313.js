6ae901981c247b2cb70bb36718ad3f7018e52072
djjr <djjrjr@gmail.com>
2020-04-22 17:33:51 -0400

    Wednesday work

---
+++ inf313.js
@@ -0,0 +1,53 @@
+var course = {"modules":[
+    {"mtitle":  "Getting Started", 
+     "iconURL": "icons/GETTING_STARTED.gif",
+     "sessions": [{"stitle": "Welcome"},
+                  {"stitle": "Quercus"},
+                  {"stitle": "Our Tech Stack"}
+                 ]
+    },
+    {"mtitle":  "Think Slowly", 
+     "iconURL": "icons/THINKSLOWLY.gif",
+     "sessions": [{"stitle": "2A"},{"stitle": "2B"}]
+    }, 
+    {"mtitle":  "Flow and Modularity", 
+     "iconURL": "icons/FLOW_MODULARITY.gif",
+     "sessions": [{"stitle": "3A"},{"stitle": "3B"}]
+    },
+    {"mtitle":  "Numbers, Logic, and Counting", 
+     "iconURL": "icons/NUMBERSLOGICCOUNTING.gif",
+     "sessions": []
+    },
+    {"mtitle":  "Information Jigs", 
+     "iconURL": "icons/INFORMATION_JIGS.gif",
+     "sessions": []
+    },
+    {"mtitle":  "Iteration", 
+     "iconURL": "icons/ITERATION.gif",
+     "sessions": []
+    },
+    {"mtitle":  "Patterns", 
+     "iconURL": "icons/PATTERNS.gif",
+     "sessions": []
+    },
+    {"mtitle":  "APIs", 
+     "iconURL": "icons/APIs.gif",
+     "sessions": []
+    },
+    {"mtitle":  "Automation", 
+     "iconURL": "icons/AUTOMATION.gif",
+     "sessions": []
+    },
+    {"mtitle":  "Blockchain", 
+     "iconURL": "icons/BLOCKCHAIN.gif",
+     "sessions": []
+    },
+    {"mtitle":  "Vision", 
+     "iconURL": "icons/VISION.gif",
+     "sessions": []
+    },
+    {"mtitle":  "ML, AI, and Robots", 
+     "iconURL": "icons/ML_AI_ROBOTS.gif",
+     "sessions": []
+    }
+    ]}
